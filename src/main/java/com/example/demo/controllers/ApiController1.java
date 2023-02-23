5cc023fe378b196662f507cb364c1a4b637bb5f6
RAGHUL005 <125949746+RAGHUL005@users.noreply.github.com>
2023-02-23 11:55:08 +0530

    Update and rename ApiController.java to ApiController1.java

--- src/main/java/com/example/demo/controllers/ApiController1.java
+++ src/main/java/com/example/demo/controllers/ApiController1.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ApiController {
-    @GetMapping("/welcome")
+public class ApiController1 {
+    @GetMapping("/1")
     public String Welcome(){
         return "Welcome String Boot!";
     }
